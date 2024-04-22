a3a0d5c0f0f34325b367343e43f71381f5ef83fa
Crowdbotics <team@crowdbotics.com>
2024-04-22 17:21:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "s-5-simplified-calc-47664",
+  url: "https://s-5-simplified-calc-47664.botics.co",
+  api: "https://s-5-simplified-calc-47664.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
